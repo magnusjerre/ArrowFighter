9aa2c09603ac31a508dd914eb1923b4c27101c93
Magnus Jerre <magnus.jerre@gmail.com>
2019-04-09 20:22:46 +0200

    Replacing the fighter jet with a square, also improving the particles look

---
+++ Assets/Scripts/PlayerScripts/PlayerColor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Jerre
+{
+    public class PlayerColor : MonoBehaviour
+    {
+        
+        void Start()
+        {
+            var playerSettings = GetComponent<PlayerSettings>();
+            if (playerSettings == null)
+            {
+                playerSettings = GetComponentInParent<PlayerSettings>();
+            }
+
+            
+
+            var renderer = GetComponent<Renderer>();
+            if (renderer == null)
+            {
+                renderer = GetComponentInParent<Renderer>();
+            }
+
+            renderer.material.color = playerSettings.color;
+        }
+    }
+}
