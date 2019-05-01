4ca2ee572c062a61db184e9df146d649f6d3b9f6
Magnus Jerre <magnus.jerre@gmail.com>
2019-05-01 16:17:40 +0200

    Moving spawn points. Adding play area indicator.

---
+++ Assets/Scripts/Utils/PlayAreaIndicator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlayAreaIndicator : MonoBehaviour
+{
+    public bool ActiveAfterStart = false;
+
+    void Start()
+    {
+        gameObject.SetActive(ActiveAfterStart);
+    }
+}
