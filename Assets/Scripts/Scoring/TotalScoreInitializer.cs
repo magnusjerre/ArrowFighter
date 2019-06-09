6ab0d6db8a6f24a025e3200300d395e9316803ce
Magnus Jerre <magnus.jerre@gmail.com>
2019-06-09 09:59:46 +0200

    Showing separate texts when game starts for free for all and undead

--- Assets/Scripts/Scoring/TotalScoreInitializer.cs
+++ Assets/Scripts/Scoring/TotalScoreInitializer.cs
@@ -1,33 +1,34 @@
-﻿using UnityEngine;
+﻿using Jerre.GameMode.FreeForAll;
+using Jerre.GameSettings;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Jerre.Scoring
 {
     public class TotalScoreInitializer : MonoBehaviour
     {
-        public Text text;
+        public Text Score;
+        public Text Title;
 
         void Awake()
         {
-            text.text = null;
+            Score.text = null;
         }
         
         void Start()
         {
-            
-        }
-
-        private void Update()
-        {
-            if (text.text == null)
+            var selectedGameMode = PlayersState.INSTANCE.selectedGameMode;
+            if (selectedGameMode == GameModes.FREE_FOR_ALL)
             {
-                var scoreManager = GameObject.FindObjectOfType<ScoreManager>();
-                if (scoreManager != null)
-                {
-                    text.text = scoreManager.maxScore + "";
-                }
+                var gameSettings = (FreeForAllGameSettings)GameSettingsState.INSTANCE.GameModeSettings;
+                Title.text = "SCORE TO WIN";
+                Score.text = gameSettings.MaxScore.ToString();
+            }
+            else if (selectedGameMode == GameModes.UNDEAD)
+            {
+                Title.text = "SURVIVE THE ROUND";
+                Score.text = "DON'T DIE AND BECOME UNDEAD";
             }
         }
-
     }
 }
