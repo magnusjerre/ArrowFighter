313c83ea6b56bcb66364aa52feb9e06477de21ca
Magnus Jerre <magnus.jerre@gmail.com>
2019-06-22 12:11:01 +0200

    Checking if a point is inside a mesh or not

---
+++ Assets/Scripts/Physics/JMeshCollisionUtil.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+
+namespace Jerre.JPhysics
+{
+    public class JMeshCollisionUtil : MonoBehaviour
+    {
+        public const float POINT_MAX_DIFF = 0.00001f;
+        public static bool IsPointInsideMesh(Vector3 point, JMesh meshTransformed)
+        {
+            // Assumes that the point is withing the AABB
+            var edgesCrossed = 0;
+            var vertices = meshTransformed.EdgeVertices;
+            var end = vertices.Length - 1;
+            Debug.Log("end: " + end);
+
+            var edgeCount = 0;
+            for (var i = 0; i < end; i++)
+            {
+                var pointA = vertices[i];
+                var pointB = vertices[i + 1];
+
+                /**
+                 * Quick check to see if the point is exactly on a vertex.
+                 * If it is, the point counts as being inside the mesh.
+                 **/
+                if (pointA.x == point.x && pointA.z == point.z)
+                {
+                    Debug.Log("Point is exactly on an existing point, should return true");
+                    return true;
+                }
+
+                
+
+                if ((pointA.z <= point.z && point.z <= pointB.z) || (pointB.z <= point.z && point.z <= pointA.z))
+                {
+                    // Can cross vertically
+                    if (pointA.x <= point.x || pointB.x <= point.x)
+                    {
+                        /** The edge starts or ends to the left of our point, 
+                         * meaning we cross the edge with our horizontal leftwards raycast 
+                         */
+
+                        /**
+                         * Check if point is on an edge. This must be done before the next type of check
+                         * that checks for a single point being added twice to edgesCrossed.
+                         **/
+                        if (CalculateSlope(pointA, pointB).IsPointOnLineInXZPlane(point, POINT_MAX_DIFF))
+                        {
+                            return true;
+                        }
+
+                        /**
+                         * Need to perform an additional check so that we don't include points crossed twice.
+                         * If our point is on the exact same "axis" (x or z position) as our end-point, don't
+                         * count it again since its been crossed when it was on pointA. 
+                         * We only count point-crossing for pointA to avoid double-counting.
+                         */
+                        if (pointB.x == point.x || pointB.z == point.z)
+                        {
+                            continue;
+                        }
+
+                        edgesCrossed++;
+                    }
+                }
+                edgeCount++;
+            }
+
+            Debug.Log("IsPointInsideMesh? edgeCount: " + edgeCount + ", edgesCrossed: " + edgesCrossed);
+            return edgesCrossed % 2 != 0;
+        }
+
+        public static Slope CalculateSlope(Vector3 pointA, Vector3 pointB)
+        {
+            var numerator = pointB.z - pointA.z;
+            var denominator = pointB.x - pointA.x;
+            if (denominator == 0f)
+            {
+                return new Slope(true, pointA.x);
+            }
+
+            var slopeGradient = numerator / denominator;
+            var slopeOffset = pointA.z - slopeGradient * pointA.x;
+            return new Slope(slopeGradient, slopeOffset);
+        }
+
+        public static bool IsPointInsideAABB(Vector3 point, Bounds bound)
+        {
+            // Ignores y-axis
+            return (bound.min.x <= point.x && point.x <= bound.max.x) && bound.min.z <= point.z && point.z <= bound.max.z;
+        }
+
+        public struct Slope
+        {
+            public float a;
+            public float b;
+
+            public bool isVertical;
+
+            public Slope(float a, float b)
+            {
+                this.a = a;
+                this.b = b;
+                isVertical = false;
+            }
+
+            public Slope(bool isVertical, float x)
+            {
+                this.isVertical = isVertical;
+                this.b = x;
+                this.a = Mathf.Infinity;
+            }
+
+            public float CalculateY(float x)
+            {
+                return a * x + b;
+            }
+
+            // Ignores Y-axis of Vector3
+            public bool IsPointOnLineInXZPlane(Vector3 point, float maxDiff)
+            {
+                if (isVertical && Mathf.Abs(point.x - b) < maxDiff)
+                {
+                    return true;
+                }
+                if (Mathf.Abs(CalculateY(point.x) - point.z) < maxDiff)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/JMeshCollisionUtilTest.cs
@@ -0,0 +1,148 @@
+﻿using Jerre.JPhysics;
+using NUnit.Framework;
+using UnityEngine;
+using static Jerre.JPhysics.JMeshCollisionUtil;
+
+namespace Tests
+{
+    public class JMeshCollisionUtilTest
+    {
+        [Test]
+        public void Point_should_be_inside_triangle()
+        {
+            var mesh = JMeshPhysicsMeshes.triangleMeshIdentity;
+            Vector3 point = new Vector3(0.5f, 0, 0.25f);
+            Assert.IsTrue(JMeshCollisionUtil.IsPointInsideMesh(point, mesh));
+            Assert.IsTrue(JMeshCollisionUtil.IsPointInsideMesh(new Vector3(1f, 0, 0), mesh));
+        }
+
+        [Test]
+        public void Point_should_be_outside_the_triangle_on_its_right()
+        {
+            var mesh = JMeshPhysicsMeshes.triangleMeshIdentity;
+            Vector3 point = new Vector3(1.25f, 0, 0.25f);
+            Assert.IsFalse(JMeshCollisionUtil.IsPointInsideMesh(point, mesh));
+        }
+
+        [Test]
+        public void Point_should_be_inside_the_transformed_square_mesh()
+        {
+            var mesh = JMeshPhysicsMeshes.squareMeshIdentity;
+            var meshFrameInstance = JMeshFrameInstance.FromMeshAndTransform(mesh, Matrix4x4.TRS(
+                new Vector3(1, 0, 1),
+                Quaternion.Euler(Vector3.up * 45),
+                Vector3.one * 2
+            ));
+
+            Assert.IsTrue(JMeshCollisionUtil.IsPointInsideMesh(new Vector3(1.001f, 0, 1f), meshFrameInstance.TransformedMesh));
+            Assert.IsTrue(JMeshCollisionUtil.IsPointInsideMesh(new Vector3(1f, 0, 1f), meshFrameInstance.TransformedMesh));
+        }
+
+        [Test]
+        public void Point_shold_be_inside_mesh_when_point_is_exactly_on_a_mesh_edge_vertex()
+        {
+            var mesh = JMeshPhysicsMeshes.squareMeshIdentity;
+            var meshFrameInstance = JMeshFrameInstance.FromMeshAndTransform(mesh, Matrix4x4.TRS(
+                new Vector3(1, 0, 1),
+                Quaternion.Euler(Vector3.up * 45),
+                Vector3.one * 2
+            ));
+
+            var instanceMesh = meshFrameInstance.TransformedMesh;
+
+            Assert.IsTrue(JMeshCollisionUtil.IsPointInsideMesh(instanceMesh.EdgeVertices[0], meshFrameInstance.TransformedMesh));
+            Assert.IsTrue(JMeshCollisionUtil.IsPointInsideMesh(instanceMesh.EdgeVertices[1], meshFrameInstance.TransformedMesh));
+            Assert.IsTrue(JMeshCollisionUtil.IsPointInsideMesh(instanceMesh.EdgeVertices[2], meshFrameInstance.TransformedMesh));
+            Assert.IsTrue(JMeshCollisionUtil.IsPointInsideMesh(instanceMesh.EdgeVertices[3], meshFrameInstance.TransformedMesh));
+        }
+
+        [Test]
+        public void Point_shold_be_inside_mesh_when_point_is_on_a_mesh_edge()
+        {
+            var mesh = JMeshPhysicsMeshes.squareMeshIdentity;
+
+            Assert.IsTrue(JMeshCollisionUtil.IsPointInsideMesh(new Vector3(0.5f, 0, 0), mesh));
+            Assert.IsTrue(JMeshCollisionUtil.IsPointInsideMesh(new Vector3(1f, 0, 0.5f), mesh));
+            Assert.IsTrue(JMeshCollisionUtil.IsPointInsideMesh(new Vector3(0.5f, 0, 1), mesh));
+            Assert.IsTrue(JMeshCollisionUtil.IsPointInsideMesh(new Vector3(0, 0, 0.5f), mesh));
+        }
+
+        [Test]
+        public void Calculate_slope_simple()
+        {
+            var slope = JMeshCollisionUtil.CalculateSlope(Vector3.zero, Vector3.one);
+            TestMethods.AreEqualIsh(1f, slope.a, 0.000001f);
+            TestMethods.AreEqualIsh(0f, slope.b, 0.000001f);
+        }
+
+        [Test]
+        public void Calculate_slope_simple_reversed()
+        {
+            var slope = JMeshCollisionUtil.CalculateSlope(Vector3.one, Vector3.zero);
+            TestMethods.AreEqualIsh(1f, slope.a, 0.000001f);
+            TestMethods.AreEqualIsh(0f, slope.b, 0.000001f);
+        }
+
+        [Test]
+        public void Calculate_slope_not_starting_in_zero()
+        {
+            var slope = JMeshCollisionUtil.CalculateSlope(Vector3.one, new Vector3(3, 0, 2));
+            TestMethods.AreEqualIsh(0.5f, slope.a, 0.00001f);
+            TestMethods.AreEqualIsh(0.5f, slope.b, 0.00001f);
+        }
+
+        [Test]
+        public void Calculate_slope_not_starting_in_zero_reversed()
+        {
+            var slope = JMeshCollisionUtil.CalculateSlope(new Vector3(3, 0, 2), Vector3.one);
+            TestMethods.AreEqualIsh(0.5f, slope.a, 0.00001f);
+            TestMethods.AreEqualIsh(0.5f, slope.b, 0.00001f);
+        }
+
+        [Test]
+        public void Calculate_vertical_slope()
+        {
+            var slope = JMeshCollisionUtil.CalculateSlope(new Vector3(1, 0, 1), new Vector3(1, 0, 2));
+            Assert.IsTrue(slope.isVertical);
+            Assert.AreEqual(1f, slope.b);
+        }
+
+        [Test]
+        public void Calculate_horizontal_slope()
+        {
+            var slope = JMeshCollisionUtil.CalculateSlope(new Vector3(1, 0, 1), new Vector3(2, 0, 1));
+            Assert.AreEqual(0f, slope.a);
+            Assert.AreEqual(1f, slope.b);
+        }
+
+        [Test]
+        public void Point_should_be_on_vertical_slope()
+        {
+            var slope = new Slope(true, 1f);
+            Assert.IsTrue(slope.IsPointOnLineInXZPlane(new Vector3(1, 0, 100), POINT_MAX_DIFF));
+            Assert.IsTrue(slope.IsPointOnLineInXZPlane(new Vector3(1, 0, -50), POINT_MAX_DIFF));
+        }
+
+        [Test]
+        public void Point_should_be_on_horizontal_slope()
+        {
+            var slope = new Slope(0f, 1f);
+            Assert.IsTrue(slope.IsPointOnLineInXZPlane(new Vector3(100, 0, 1), POINT_MAX_DIFF));
+            Assert.IsTrue(slope.IsPointOnLineInXZPlane(new Vector3(-10, 0, 1), POINT_MAX_DIFF));
+        }
+
+        [Test]
+        public void Point_should_be_on_slanted_slope()
+        {
+            var slope = new Slope(1f, 0f);
+            Assert.IsTrue(slope.IsPointOnLineInXZPlane(new Vector3(1f, 0, 1f), POINT_MAX_DIFF));
+        }
+
+        [Test]
+        public void Point_should_not_be_on_slanted_slope()
+        {
+            var slope = new Slope(1f, 0f);
+            Assert.IsFalse(slope.IsPointOnLineInXZPlane(new Vector3(2f, 0, 1f), POINT_MAX_DIFF));
+        }
+    }
+}
