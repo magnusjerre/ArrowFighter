c47d5e50bc569107cdc2a1e2d8bdfe3a97969a9d
Magnus Jerre <magnus.jerre@gmail.com>
2019-06-27 19:31:45 +0200

    Successfully checking whether two meshes overlap or not

--- Assets/Scripts/Physics/Utils/JMeshOverlap.cs
+++ Assets/Scripts/Physics/Utils/JMeshOverlap.cs
@@ -6,6 +6,8 @@ namespace Jerre.JPhysics
 {
     public class JMeshOverlap
     {
+        public const float POINT_MAX_DIFF = 0.00001f;
+
         public static bool MeshesOverlap(JMesh meshA, JMesh meshB)
         {
             if (!AABBOverlap(meshA.AABB, meshB.AABB)) return false;
@@ -14,6 +16,8 @@ namespace Jerre.JPhysics
             var aLength = meshA.EdgeVertices.Length - 1;
             var bVertices = meshB.EdgeVertices;
             var bLength = meshB.EdgeVertices.Length - 1;
+
+            // Check for any edges crossing
             for (var i = 0; i < aLength; i++)
             {
                 var aStart = aVertices[i];
@@ -31,6 +35,26 @@ namespace Jerre.JPhysics
                     }
                 }
             }
+
+            Debug.Log("No lines crossed, looking for points inside each mesh");
+
+            // Check if any vertices from meshA is inside meshB
+            for (var i = 0; i < aLength; i++)
+            {
+                var pointA = aVertices[i];
+                if (IsPointInsideMesh(pointA, meshB)) return true;
+            }
+
+            Debug.Log("No points from meshA inside meshB");
+
+            for (var i = 0; i < bLength; i++)
+            {
+                var pointB = bVertices[i];
+                if (IsPointInsideMesh(pointB, meshA)) return true;
+            }
+
+            Debug.Log("No points from meshB inside meshA");
+            Debug.Log("No mesh overlap");
 
             return false;
         }
@@ -71,6 +95,88 @@ namespace Jerre.JPhysics
             var minBZ = Mathf.Min(bStart.z, bEnd.z);
             var maxBZ = Mathf.Max(bStart.z, bEnd.z);
             return !(p.x < minBX || p.x > maxBX || p.z < minBZ || p.z > maxBZ);
+        }
+
+        public static bool IsPointInsideMesh(Vector3 point, JMesh meshTransformed)
+        {
+            // Assumes that the point is withing the AABB
+            var edgesCrossed = 0;
+            var vertices = meshTransformed.EdgeVertices;
+            var end = vertices.Length - 1;
+
+            var horizontalPointSlope = Slope.FromPoints(point, new Vector3(point.x - 1, 0, point.z));
+            var endOfRayCast = new Vector3(meshTransformed.AABB.min.x - 1f, 0, point.z);
+
+            for (var i = 0; i < end; i++)
+            {
+                var pointA = vertices[i];
+                var pointB = vertices[i + 1];
+
+                /**
+                 * Quick check to see if the point is exactly on a vertex.
+                 * If it is, the point counts as being inside the mesh.
+                 **/
+                if (pointA.x == point.x && pointA.z == point.z)
+                {
+                    Debug.Log("Point is exactly on an existing point, should return true");
+                    return true;
+                }
+
+                var edgeSlope = Slope.FromPoints(pointA, pointB);
+                if (edgeSlope.IsPointOnLineInXZPlane(point, POINT_MAX_DIFF))
+                {
+                    Debug.Log("Point is on line in XZ plane, edge: " + (pointB - pointA));
+                    return true;
+                }
+
+                var intersection = edgeSlope.CalculateIntersection(horizontalPointSlope);
+                if (LinesCross(point, endOfRayCast, pointA, pointB))
+                {
+                    Debug.Log("Crossed edge! " + (pointB - pointA));
+                    edgesCrossed++;
+                }
+
+
+                //if ((pointA.z <= point.z && point.z <= pointB.z) || (pointB.z <= point.z && point.z <= pointA.z))
+                //{
+                //    // Can cross vertically
+                //    if (pointA.x <= point.x || pointB.x <= point.x)
+                //    {
+                //        /** The edge starts or ends to the left of our point, 
+                //         * meaning we cross the edge with our horizontal leftwards raycast 
+                //         */
+
+                //        /**
+                //         * Check if point is on an edge. This must be done before the next type of check
+                //         * that checks for a single point being added twice to edgesCrossed.
+                //         **/
+                //        var slope = Slope.FromPoints(pointA, pointB);
+                //        if (slope.IsPointOnLineInXZPlane(point, POINT_MAX_DIFF))
+                //        {
+                //            return true;
+                //        }
+
+                //        /**
+                //         * Need to perform an additional check so that we don't include points crossed twice.
+                //         * If our point is on the exact same "axis" (x or z position) as our end-point, don't
+                //         * count it again since its been crossed when it was on pointA. 
+                //         * We only count point-crossing for pointA to avoid double-counting.
+                //         */
+                //        if (pointB.x == point.x && pointB.z == point.z)
+                //        {
+                //            continue;
+                //        }
+
+                //        var xIntersection = slope.CalculateIntersectionWithHorizontalLine(point.z);
+                //        var minX = Mathf.Min(pointA.x, pointB.x);
+                //        if (minX <= xIntersection && xIntersection <= point.x)
+                //        {
+                //            edgesCrossed++;
+                //        }
+                //    }
+                //}
+            }
+            return edgesCrossed % 2 != 0;
         }
 
     }
--- Assets/Scripts/Tests/JMeshOverlapTest.cs
+++ Assets/Scripts/Tests/JMeshOverlapTest.cs
@@ -25,6 +25,18 @@ namespace Tests
         public static int[] trianglesB = new int[] { 0, 1, 2 };
         public static JMesh meshB = JMesh.CalculateJMesh(triangleB, trianglesB);
 
+        public static Vector3[] triangleC = new Vector3[]
+        {
+            new Vector3(2, 0, 3),
+            new Vector3(3, 0, 1),
+            new Vector3(4, 0, 4)
+        };
+        public static int[] trianglesC = new int[]
+        {
+            0, 1, 2
+        };
+        public static JMesh meshC = JMesh.CalculateJMesh(triangleC, trianglesC);
+
         public static Vector3[] square = new Vector3[]
         {
             new Vector3(0, 0, 0),
@@ -112,6 +124,8 @@ namespace Tests
 
             Assert.IsFalse(JMeshOverlap.LinesCross(new Vector3(5, 0, 4), new Vector3(9, 0, 1), new Vector3(4, 0, 0), new Vector3(5, 0, 7)));
             Assert.IsFalse(JMeshOverlap.LinesCross(new Vector3(4, 0, 0), new Vector3(5, 0, 7), new Vector3(5, 0, 4), new Vector3(9, 0, 1)));
+
+            Assert.IsFalse(JMeshOverlap.LinesCross(new Vector3(0, 0, 2), new Vector3(5, 0, 7), new Vector3(10, 0, 3), new Vector3(5, 0, 4)));
         }
 
         [Test]
@@ -125,6 +139,81 @@ namespace Tests
         public void Meshes_should_overlap_without_having_a_point_inside_the_other()
         {
             Assert.IsTrue(JMeshOverlap.MeshesOverlap(meshA, meshB));
+        }
+
+        [Test]
+        public void Meshes_should_overlap_when_a_smaller_mesh_is_completely_inside_a_larger_mesh()
+        {
+            Assert.IsTrue(JMeshOverlap.MeshesOverlap(meshA, meshC));
+            Assert.IsTrue(JMeshOverlap.MeshesOverlap(meshC, meshA));
+        }
+
+        [Test]
+        public void Point_should_be_inside_triangle()
+        {
+            var mesh = JMeshPhysicsMeshes.triangleMeshIdentity;
+            Vector3 point = new Vector3(0.5f, 0, 0.25f);
+            Assert.IsTrue(JMeshOverlap.IsPointInsideMesh(point, mesh));
+            Assert.IsTrue(JMeshOverlap.IsPointInsideMesh(new Vector3(1f, 0, 0), mesh));
+        }
+
+        [Test]
+        public void Point_should_be_outside_the_triangle_on_its_right()
+        {
+            var mesh = JMeshPhysicsMeshes.triangleMeshIdentity;
+            Vector3 point = new Vector3(1.25f, 0, 0.25f);
+            Assert.IsFalse(JMeshOverlap.IsPointInsideMesh(point, mesh));
+        }
+
+        [Test]
+        public void Point_should_be_outside_the_triangle_since_the_horizontal_raycast_touches_a_single_vertex()
+        {
+            var mesh = meshA;
+            var point = new Vector3(mesh.EdgeVertices[1].x + 0.1f, 0, mesh.EdgeVertices[1].z);
+            Assert.IsFalse(JMeshOverlap.IsPointInsideMesh(point, mesh));
+        }
+
+        [Test]
+        public void Point_should_be_inside_the_transformed_square_mesh()
+        {
+            var mesh = JMeshPhysicsMeshes.squareMeshIdentity;
+            var meshFrameInstance = JMesh.FromMeshAndTransform(mesh, Matrix4x4.TRS(
+                new Vector3(1, 0, 1),
+                Quaternion.Euler(Vector3.up * 45),
+                Vector3.one * 2
+            ));
+
+            Assert.IsTrue(JMeshOverlap.IsPointInsideMesh(new Vector3(1.001f, 0, 1f), meshFrameInstance));
+            Assert.IsTrue(JMeshOverlap.IsPointInsideMesh(new Vector3(1f, 0, 1f), meshFrameInstance));
+        }
+
+        [Test]
+        public void Point_shold_be_inside_mesh_when_point_is_exactly_on_a_mesh_edge_vertex()
+        {
+            var mesh = JMeshPhysicsMeshes.squareMeshIdentity;
+            var meshFrameInstance = JMesh.FromMeshAndTransform(mesh, Matrix4x4.TRS(
+                new Vector3(1, 0, 1),
+                Quaternion.Euler(Vector3.up * 45),
+                Vector3.one * 2
+            ));
+
+            var instanceMesh = meshFrameInstance;
+
+            Assert.IsTrue(JMeshOverlap.IsPointInsideMesh(instanceMesh.EdgeVertices[0], meshFrameInstance));
+            Assert.IsTrue(JMeshOverlap.IsPointInsideMesh(instanceMesh.EdgeVertices[1], meshFrameInstance));
+            Assert.IsTrue(JMeshOverlap.IsPointInsideMesh(instanceMesh.EdgeVertices[2], meshFrameInstance));
+            Assert.IsTrue(JMeshOverlap.IsPointInsideMesh(instanceMesh.EdgeVertices[3], meshFrameInstance));
+        }
+
+        [Test]
+        public void Point_shold_be_inside_mesh_when_point_is_on_a_mesh_edge()
+        {
+            var mesh = JMeshPhysicsMeshes.squareMeshIdentity;
+
+            Assert.IsTrue(JMeshOverlap.IsPointInsideMesh(new Vector3(0.5f, 0, 0), mesh));
+            Assert.IsTrue(JMeshOverlap.IsPointInsideMesh(new Vector3(1f, 0, 0.5f), mesh));
+            Assert.IsTrue(JMeshOverlap.IsPointInsideMesh(new Vector3(0.5f, 0, 1), mesh));
+            Assert.IsTrue(JMeshOverlap.IsPointInsideMesh(new Vector3(0, 0, 0.5f), mesh));
         }
     }
 }
