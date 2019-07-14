3158fa96e5d0c5031a3896f112834311fdcaf7a9
Magnus Jerre <magnus.jerre@gmail.com>
2019-07-14 20:02:40 +0200

    Moving DoCall to Utils

---
+++ Assets/Scripts/Utils/DoCall.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Jerre.Utils
+{
+    public class DoCall : MonoBehaviour
+    {
+        public float Delay = 2f;
+        // Start is called before the first frame update
+        void Start()
+        {
+            Invoke("Delayed", Delay);   
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        void Delayed() {
+            var doCallers = GetComponents<IDo>();
+            for (var i = 0; i < doCallers.Length; i++) {
+                doCallers[i].Do();
+            }
+        }
+    }
+}
