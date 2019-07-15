d9c2f36c8148795131a3fb42b4878d36adcdd9f3
Magnus Jerre <magnus.jerre@gmail.com>
2019-07-15 10:16:21 +0200

    Renaming TotalScoreInitializer to SplashDesricptionForGameMode.

---
+++ Assets/Scripts/SceneSpecificScripts/InGame/SplashDescriptionForGameMode.cs
@@ -0,0 +1,35 @@
+﻿using Jerre.GameMode;
+using Jerre.GameMode.FreeForAll;
+using Jerre.GameSettings;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Jerre.Scoring
+{
+    public class SplashDescriptionForGameMode : MonoBehaviour
+    {
+        public Text Description;
+        public Text Title;
+
+        void Awake()
+        {
+            Description.text = null;
+        }
+        
+        void Start()
+        {
+            var selectedGameMode = PlayersState.INSTANCE.selectedGameMode;
+            if (selectedGameMode == GameModes.FREE_FOR_ALL)
+            {
+                var gameSettings = (FreeForAllGameSettings)GameSettingsState.INSTANCE.GameModeSettings;
+                Title.text = "SCORE TO WIN";
+                Description.text = gameSettings.MaxScore.ToString();
+            }
+            else if (selectedGameMode == GameModes.UNDEAD)
+            {
+                Title.text = "SURVIVE THE ROUND";
+                Description.text = "DON'T DIE AND BECOME UNDEAD";
+            }
+        }
+    }
+}
