ec77e452bc2d45bef56dece62b8ab1d9c5393eb8
Magnus Jerre <magnus.jerre@gmail.com>
2019-07-15 10:40:20 +0200

    Moving ScoreSummary to InGame folder

---
+++ Assets/Scripts/SceneSpecificScripts/InGame/ScoreSummary.cs
@@ -0,0 +1,59 @@
+﻿using Jerre.Events;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Jerre.Scoring
+{
+    public class ScoreSummary : MonoBehaviour, IAFEventListener
+    {
+        public Text leaderScoreText, totalScoreText;
+
+        private int leaderScore;
+        private int leaderPlayerNumber;
+        private Dictionary<int, Color> playerColorMap;
+
+        void Awake() {
+            AFEventManager.INSTANCE.AddListener(this);
+            InitializeColorMap();
+        }
+        
+        void Start()
+        {
+            leaderScoreText.text = "0";
+            var scoreManager = GameObject.FindObjectOfType<FreeForAllGameModeManager>();
+            if (scoreManager != null)
+            {
+                totalScoreText.text = scoreManager.maxScore + "";
+            }
+        }
+
+        public bool HandleEvent(AFEvent afEvent)
+        {
+            switch(afEvent.type) {
+                case AFEventType.SCORE: {
+                    var payload = (ScorePayload)afEvent.payload;
+                    if (payload.playerScore > leaderScore) {
+                        leaderScore = payload.playerScore;
+                        leaderPlayerNumber = payload.playerNumber;
+                        if (!playerColorMap.ContainsKey(leaderPlayerNumber)) {
+                            InitializeColorMap();
+                        }
+                        leaderScoreText.color = playerColorMap[leaderPlayerNumber];
+                        leaderScoreText.text = leaderScore + "";
+                    }
+                    break;
+                }
+            }
+            return false;
+        }
+        void InitializeColorMap() {
+            playerColorMap = new Dictionary<int, Color>();
+            for (var i = 0; i < PlayersState.INSTANCE.ReadyPlayersCount; i++)
+            {
+                var playerMenuSettings = PlayersState.INSTANCE.GetSettings(i);
+                playerColorMap.Add(playerMenuSettings.Number, playerMenuSettings.Color);
+            }
+        }
+    }
+}
